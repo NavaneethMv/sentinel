e7f4ef3fd4de6afd8c5f40b763a6c99621470ad7
NavaneethMv <mvnavaneeth5@gmail.com>
2026-03-06 17:06:20 +0530

    Refactor main function to handle command-line arguments and improve usage instructions

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+from parser import parse_file
+import sys
+
+
 def main():
-    print("Hello from sentinel!")
+    if (len(sys.argv) == 2):
+        targets = sys.argv[1:] 
+        
+    else:
+        print("Usage: python main.py <file_name/file_folder>")
+        sys.exit(1)
 
 
 if __name__ == "__main__":

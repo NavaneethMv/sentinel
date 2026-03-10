c4f4f18af5c1086476b1a8304b1114f68acdf624
NavaneethMv <mvnavaneeth5@gmail.com>
2026-03-10 16:55:06 +0530

    feat: Implement rule loading and transformation for taint analysis

---
+++ dsl.py
@@ -0,0 +1,33 @@
+from lark import Lark, Transformer
+from dataclasses import dataclass
+
+@dataclass
+class Rule:
+    source:str
+    sink: str
+
+class RuleTransformer(Transformer):
+    def rule(self, items):
+        return Rule(source=str(items[0].children[0]),
+                    sink=str(items[1].children[0])) 
+
+    def start(self, items):
+        return items
+
+
+def load_rules(path: str) -> list[Rule]:
+    with open("grammer.lark", "r") as f:
+        grammer = f.read()
+    
+    with open(path, "r") as f:
+        rules = f.read()
+    
+    parser = Lark(grammer)
+    tree = parser.parse(rules)
+    return RuleTransformer().transform(tree)
+
+
+if __name__ == "__main__":
+    rules = load_rules("sentinel.rules")
+    for r in rules:
+        print(f"Rule: {r.source} must never reach {r.sink}")
